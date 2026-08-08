93221b3f3362459bbba45e026deaa6f597b33788
LilPipinder <flashsanek8@gmail.com>
2026-08-08 15:28:10 +0300

    Маршрутизация

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { CocktailList } from './components/cocktail-list/cocktail-list';
+import { CocktailForm } from './components/cocktail-form/cocktail-form';
+import { CocktailDetail } from './components/cocktail-detail/cocktail-detail';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: CocktailList },
+  { path: 'create', component: CocktailForm },
+  { path: 'edit/:id', component: CocktailForm },
+  { path: 'view/:id', component: CocktailDetail },
+  { path: '**', redirectTo: '' }
+];
